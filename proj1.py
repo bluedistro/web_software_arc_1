779f2d0b6485922117feb925e346d6be7b107dad
captainburnaby <bineykingsley36@gmail.com>
2018-02-06 12:49:03 +0000

    initial commit

---
+++ proj1.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, url_for, redirect
+import werkzeug
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template('header.html')
+
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
+
+
+if __name__ == '__main__':
+    app.run()
