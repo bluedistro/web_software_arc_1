fdd4aceaac74de31f170ddc6c973fdc8d4690c42
captainburnaby <bineykingsley36@gmail.com>
2018-02-08 14:56:56 +0000

    login validation

--- database_setup.py
+++ database_setup.py
@@ -1,29 +1,9 @@
 import pymongo
+import pymongo.errors
 from pymongo import MongoClient
 import datetime
 from werkzeug.security import generate_password_hash
 from werkzeug.security import check_password_hash
-
-class User():
-
-    def __init__(self, username):
-        self.username = username
-
-    def is_authenticated(self):
-        return True
-
-    def is_active(self):
-        return True
-
-    def is_anonymous(self):
-        return False
-
-    def get_id(self):
-        return self.username
-
-    @staticmethod
-    def validate_login(password_hash, password):
-        return check_password_hash(password_hash, password)
 
 
 class database:
@@ -32,8 +12,8 @@ class database:
 
         self.client = MongoClient()
         self.database = self.client[database]
-        self.users = self.database[user_collection]
-
+        self.user_collection = user_collection
+        self.users = self.database[self.user_collection]
 
     def register_user(self, firstname, lastname, email, password):
         success = False
@@ -44,8 +24,55 @@ class database:
             "email": email,
             "password": password_hash
         }
-        user_id = self.users.insert_one(user_info).inserted_id
+        status = self.validate_email(email=email)
+        if firstname and lastname and email and password is not None:
+            if status is not "Email already exist!, please use another email":
+                user_id = self.users.insert_one(user_info).inserted_id
+            else:
+                success = False
+                user_id = None
+                pass
 
         if user_id != None:
             success = True
         return user_id, success
+
+    # temporal validation of email before entry for both live search and final entry
+    def validate_email(self, email):
+        documents = []
+        collection = self.database['users']
+        cursor = collection.find({})
+        for document in cursor:
+            documents.append(document['email'])
+
+        status = "Email has already been registered!, please use another email"
+        message = [status for em in documents if unicode(email).lower() in em.lower()]
+        return message
+
+    def login_user(self, email, password):
+        emails_doc = []
+        passwords_doc = []
+        # password_enc = generate_password_hash('Kewl4life!')
+        # decoded = check_password_hash(password_enc, password)
+        collection = self.database['users']
+        cursor = collection.find({})
+        for document in cursor:
+            emails_doc.append(document['email'])
+            passwords_doc.append(document['password'])
+
+        # data = zip(emails_doc, passwords_doc)
+        # print(data)
+        for e, p in zip(emails_doc, passwords_doc):
+            if e == unicode(email) and check_password_hash(p, unicode(password)) is True:
+                print('found match')
+                status = True
+                break
+            else:
+                print('Match not found')
+                status = False
+
+
+        return status
+
+# db = database(database='wsa', user_collection='users')
+# print(db.login_user('bineykingsley36@gmail.com', 'Kewl4life!'))
--- proj1.py
+++ proj1.py
@@ -1,10 +1,69 @@
-from flask import Flask, render_template, request, url_for, flash, redirect
-import werkzeug
+from flask import Flask, render_template, request, url_for, flash, redirect, session, abort, Response
+from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user
+import json
+
 from database_setup import database
 
+# config
 app = Flask(__name__)
-app.secret_key = 'some_secret'
+app.secret_key = 'some_secret_xxx'
 db = database(database='wsa', user_collection='users')
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+class User(UserMixin):
+
+    def __init__(self, email):
+        self.id = email
+
+    def __repr__(self):
+        return "%s%s" % (self.id)
+
+# protected url
+@app.route('/protected')
+@login_required
+def protected():
+    return render_template('protecteed_test.html')
+
+@app.route('/login_try/')
+def login_try():
+    return render_template('login.html')
+
+# dummy login page
+@app.route('/login/', methods=['GET', 'POST'])
+def login_alt():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        status = db.login_user(email=username, password=password)
+        next = request.args.get('protected')
+        if status:
+            user = User(username)
+            login_user(user)
+            return redirect(next or url_for('protected'))
+        else:
+            return abort(401)
+    else:
+        return render_template('login.html')
+
+@app.route('/logout/')
+@login_required
+def logout():
+    logout_user()
+    return render_template('login.html')
+
+
+@app.errorhandler(401)
+def page_not_found(e):
+    # return Response('<p>Login Failed</p>')
+    return render_template('login.html')
+
+
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
 
 @app.route('/')
 def home_1():
@@ -14,9 +73,15 @@ def home_1():
 def home():
     return render_template('header.html')
 
-@app.route('/login/', methods=['GET', 'POST'])
+@app.route('/login_alt/', methods=['GET', 'POST'])
 def login():
     return render_template('header.html')
+
+@app.route('/email_validate/', methods=['POST', 'GET'])
+def email_validate():
+    email = request.args['email']
+    message = database.validate_email(db, email=email)
+    return json.dumps({"results": message})
 
 @app.route('/signup/', methods=['GET', 'POST'])
 def signup():
@@ -32,10 +97,10 @@ def signup():
             message = 'Registration failed. Passwords do not match'
             print(message)
             flash(message)
-            return redirect(url_for('home'))
+            return redirect(url_for('home'), code=302)
         else:
-            user_id, success = db.register_user(firstname=first_name, lastname=last_name, email=email, password=password)
-            if success == True:
+            user, success = db.register_user(firstname=first_name, lastname=last_name, email=email, password=password)
+            if success:
                 message = 'Registration successful'
                 print(message)
                 flash(message)
@@ -44,7 +109,8 @@ def signup():
                 flash(message)
                 print(message)
 
-            return redirect(url_for('home'))
+            return redirect(url_for('home'), code=302)
+
 
 
 if __name__ == '__main__':
