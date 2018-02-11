0d03552b727f363e9e0b38efedf5edfbb72d70ec
captainburnaby <bineykingsley36@gmail.com>
2018-02-11 12:13:58 +0000

    completed database management

--- proj1.py
+++ proj1.py
@@ -13,6 +13,11 @@ app.secret_key = 'some_secret_xxx'
 db = database(db='wsa', user_collection='users')
 
 bdr_db = database(db='bdr', user_collection='members')
+nhis_db = database(db='nhis', user_collection='members')
+dvla_db = database(db='dvla', user_collection='members')
+gec_db = database(db='gec', user_collection='members')
+nia_db = database(db='nia', user_collection='members')
+gps_db = database(db='gps', user_collection='members')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -114,18 +119,9 @@ def email_validate():
     message = database.validate_email(db, email=email)
     return json.dumps({"results": message})
 
-@app.route('/panels_wells/', methods=['POST', 'GET'])
-def panels_wells():
-    return render_template('panels-wells.html')
 
-@app.route('/notifications/', methods=['POST', 'GET'])
-def notifications():
-    return render_template('notifications.html')
-
-@app.route('/blank/', methods=['POST', 'GET'])
-def blank():
-    return render_template('blank.html')
-
+# --------------- DATABASE ACCESS AND INTERACTIONS --------------------------
+# Birth and Death Routing
 @app.route('/bdr/', methods=['POST', 'GET'])
 def bdr():
     if request.method == 'POST':
@@ -162,25 +158,192 @@ def bdr():
 
     return render_template('bdr.html')
 
+# Driver and Vehicle Licensing Authority Routing
 @app.route('/dvla/', methods=['POST', 'GET'])
 def dvla():
+    if request.method == 'POST':
+        firstname = request.form['db_firstname']
+        try:
+            middlename = request.form['db_middlename']
+        except Exception as e:
+            print(str(e))
+            middlename = ''
+        lastname = request.form['db_lastname']
+        dob = request.form['date']
+        birthplace = request.form['db_birthplace']
+
+        if firstname and lastname and dob and birthplace is not None:
+            user_id, success = dvla_db.db_member_registration(firstname=firstname, middlename=middlename,
+                                                             lastname=lastname, dob=dob,
+                                                             birthplace=birthplace)
+            flash('User has been registered successfully!')
+            return render_template('dvla.html')
+        else:
+            message = 'Registration failed'
+            flash(message)
+            print(message)
+            return render_template('dvla.html')
+
+    elif request.method == 'GET':
+        firstnames, middlenames, lastnames, dobs, birthplaces = dvla_db.fetch_db_members()
+        items = []
+        for fn, mn, ln, dob, bp in zip(firstnames, middlenames, lastnames, dobs, birthplaces):
+            an_item = dict(firstname=fn, middlename=mn, lastname=ln, dob=dob, birthplace=bp)
+            items.append(an_item)
+
+        return render_template('dvla.html', items=items)
+
     return render_template('dvla.html')
 
+# Ghana Electoral Commission Routing
 @app.route('/gec/', methods=['POST', 'GET'])
 def gec():
+    if request.method == 'POST':
+        firstname = request.form['db_firstname']
+        try:
+            middlename = request.form['db_middlename']
+        except Exception as e:
+            print(str(e))
+            middlename = ''
+        lastname = request.form['db_lastname']
+        dob = request.form['date']
+        birthplace = request.form['db_birthplace']
+
+        if firstname and lastname and dob and birthplace is not None:
+            user_id, success = gec_db.db_member_registration(firstname=firstname, middlename=middlename,
+                                                             lastname=lastname, dob=dob,
+                                                             birthplace=birthplace)
+            flash('User has been registered successfully!')
+            return render_template('gec.html')
+        else:
+            message = 'Registration failed'
+            flash(message)
+            print(message)
+            return render_template('gec.html')
+
+    elif request.method == 'GET':
+        firstnames, middlenames, lastnames, dobs, birthplaces = gec_db.fetch_db_members()
+        items = []
+        for fn, mn, ln, dob, bp in zip(firstnames, middlenames, lastnames, dobs, birthplaces):
+            an_item = dict(firstname=fn, middlename=mn, lastname=ln, dob=dob, birthplace=bp)
+            items.append(an_item)
+
+        return render_template('gec.html', items=items)
+
     return render_template('gec.html')
 
+# Ghana Passport Service Routing
 @app.route('/gps/', methods=['POST', 'GET'])
 def gps():
+    if request.method == 'POST':
+        firstname = request.form['db_firstname']
+        try:
+            middlename = request.form['db_middlename']
+        except Exception as e:
+            print(str(e))
+            middlename = ''
+        lastname = request.form['db_lastname']
+        dob = request.form['date']
+        birthplace = request.form['db_birthplace']
+
+        if firstname and lastname and dob and birthplace is not None:
+            user_id, success = gps_db.db_member_registration(firstname=firstname, middlename=middlename,
+                                                              lastname=lastname, dob=dob,
+                                                              birthplace=birthplace)
+            flash('User has been registered successfully!')
+            return render_template('gps.html')
+        else:
+            message = 'Registration failed'
+            flash(message)
+            print(message)
+            return render_template('gps.html')
+
+    elif request.method == 'GET':
+        firstnames, middlenames, lastnames, dobs, birthplaces = gps_db.fetch_db_members()
+        items = []
+        for fn, mn, ln, dob, bp in zip(firstnames, middlenames, lastnames, dobs, birthplaces):
+            an_item = dict(firstname=fn, middlename=mn, lastname=ln, dob=dob, birthplace=bp)
+            items.append(an_item)
+
+        return render_template('gps.html', items=items)
+
     return render_template('gps.html')
 
+# National Health Insurance Scheme Routing
 @app.route('/nhis/', methods=['POST', 'GET'])
 def nhis():
+    if request.method == 'POST':
+        firstname = request.form['db_firstname']
+        try:
+            middlename = request.form['db_middlename']
+        except Exception as e:
+            print(str(e))
+            middlename = ''
+        lastname = request.form['db_lastname']
+        dob = request.form['date']
+        birthplace = request.form['db_birthplace']
+
+        if firstname and lastname and dob and birthplace is not None:
+            user_id, success = nhis_db.db_member_registration(firstname=firstname, middlename=middlename,
+                                                             lastname=lastname, dob=dob,
+                                                             birthplace=birthplace)
+            flash('User has been registered successfully!')
+            return render_template('nhis.html')
+        else:
+            message = 'Registration failed'
+            flash(message)
+            print(message)
+            return render_template('nhis.html')
+
+    elif request.method == 'GET':
+        firstnames, middlenames, lastnames, dobs, birthplaces = nhis_db.fetch_db_members()
+        items = []
+        for fn, mn, ln, dob, bp in zip(firstnames, middlenames, lastnames, dobs, birthplaces):
+            an_item = dict(firstname=fn, middlename=mn, lastname=ln, dob=dob, birthplace=bp)
+            items.append(an_item)
+
+        return render_template('nhis.html', items=items)
+
     return render_template('nhis.html')
 
+# National Identification Authority Routing
 @app.route('/nia/', methods=['POST', 'GET'])
 def nia():
+    if request.method == 'POST':
+        firstname = request.form['db_firstname']
+        try:
+            middlename = request.form['db_middlename']
+        except Exception as e:
+            print(str(e))
+            middlename = ''
+        lastname = request.form['db_lastname']
+        dob = request.form['date']
+        birthplace = request.form['db_birthplace']
+
+        if firstname and lastname and dob and birthplace is not None:
+            user_id, success = nia_db.db_member_registration(firstname=firstname, middlename=middlename,
+                                                             lastname=lastname, dob=dob,
+                                                             birthplace=birthplace)
+            flash('User has been registered successfully!')
+            return render_template('nia.html')
+        else:
+            message = 'Registration failed'
+            flash(message)
+            print(message)
+            return render_template('nia.html')
+
+    elif request.method == 'GET':
+        firstnames, middlenames, lastnames, dobs, birthplaces = nia_db.fetch_db_members()
+        items = []
+        for fn, mn, ln, dob, bp in zip(firstnames, middlenames, lastnames, dobs, birthplaces):
+            an_item = dict(firstname=fn, middlename=mn, lastname=ln, dob=dob, birthplace=bp)
+            items.append(an_item)
+
+        return render_template('nia.html', items=items)
+
     return render_template('nia.html')
+
+# ---------------------- END OF DATABASE ACCESS AND ROUTING -------------------------
 
 
 @app.route('/signup/', methods=['GET', 'POST'])
